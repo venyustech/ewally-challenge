5cbea98a5315b8da8c6ef3b23c9db2912c5c1e3f
Venyus Boldrini <venyustech@gmail.com>
2023-08-12 02:50:04 -0300

    feat: add recommendation route
    Signed-off-by: Venyus Boldrini <venyustech@gmail.com>

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import personRouter from "./userRouter.js";
 import removedb from "./removeAllDatabase.js";
 import relationshipRouter from "./relathionshipRouter.js";
+import recommendationsRouter from "./recommendationsRouter.js";
 
 const router = Router();
 router.use(personRouter);
 router.use(removedb);
 router.use(relationshipRouter);
+router.use(recommendationsRouter);
 
 export default router;
